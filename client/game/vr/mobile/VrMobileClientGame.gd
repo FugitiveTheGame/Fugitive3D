efbb58ccd87ed076b60e6426e4ff029d2aa25952
Adam Brown <adamwbrown@gmail.com>
2020-03-27 11:31:17 -0700

    PC VR is now working!

--- client/game/vr/mobile/VrMobileClientGame.gd
+++
@@ -1,6 +0,0 @@
-extends "res://client/game/ClientGame.gd"
-
-
-func create_player_node() -> Node:
-	var scene = preload("res://client/game/player/vr/mobile/VrMobilePlayer.tscn")
-	return scene.instance()
