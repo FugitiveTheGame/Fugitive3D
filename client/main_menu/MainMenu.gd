0fe3b842d72c1895b355f0bd66c6862edbe15fc1
Adam Brown <adamwbrown@gmail.com>
2020-03-26 19:39:53 -0700

    Initial commit

---
+++ client/main_menu/MainMenu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+func _ready():
+	get_tree().connect('connected_to_server', self, 'on_connected_to_server')
+
+
+func _on_ConnectButton_pressed():
+	var ip := $ServerIpLabel/ServerIp.text as String
+	var playerName := $PlayerNameLabel/PlayerName.text as String
+	connect_to_server(playerName, ip)
+
+
+func connect_to_server(playerName: String, serverIp: String):
+	ClientNetwork.join_game(serverIp, playerName)
+
+
+func on_connected_to_server():
+	go_to_lobby()
+
+
+func go_to_lobby():
+	print("go_to_lobby() MUST BE OVERRIDEN")
---
+++ networking/ServerNetwork.gd
@@ -0,0 +1,52 @@
+extends "BaseNetwork.gd"
+
+const SERVER_ID := 1
+
+func _player_connected(id):
+	print("Player connected: " + str(id))
+
+
+# Called by clients when they connect
+func register_self(playerId: int, playerName: String):
+	rpc_id(SERVER_ID, "on_register_self", playerId, playerName)
+
+
+remote func on_register_self(playerId, playerName):
+	# Register this client with the server
+	ClientNetwork.on_register_player(playerId, playerName)
+	
+	# Register the new player with all existing clients
+	for curPlayerId in GameData.players:
+		ClientNetwork.register_player(curPlayerId, playerId, playerName)
+	
+	# Catch the new player up on who is already here
+	for curPlayerId in GameData.players:
+		if curPlayerId != playerId:
+			var player = GameData.players[curPlayerId]
+			ClientNetwork.register_player(playerId, curPlayerId, player.name)
+
+
+func is_hosting() -> bool:
+	if get_tree().network_peer != null and get_tree().network_peer.get_connection_status() != 0: # NetworkedMultiplayerPeer.ConnectionStatus.CONNECTION_DISCONNECTED
+		return true
+	else:
+		return false
+
+
+func host_game() -> bool:
+	
+	# Clear out any old state
+	reset_network()
+	
+	var peer = NetworkedMultiplayerENet.new()
+	var result = peer.create_server(SERVER_PORT, MAX_PLAYERS)
+	if result == OK:
+		get_tree().set_network_peer(peer)
+		
+		get_tree().connect("network_peer_connected", self, "_player_connected")	
+		
+		print("Server started.")
+		return true
+	else:
+		print("Failed to host game: %d" % result)
+		return false
