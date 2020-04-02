4acde1b70ed1253b572acd845f367bdd50340518
Adam Brown <adamwbrown@gmail.com>
2020-04-02 14:06:55 -0700

    Crouching implemented for server and Flat clients

--- client/game/player/controller/vr/VrPlayerController.gd
+++ client/game/player/controller/vr/VrPlayerController.gd
@@ -1,1 +1,5 @@
 extends "res://addons/OQ_Toolkit/OQ_ARVROrigin/scripts/OQ_ARVROrigin.gd"
+
+
+func _physics_process(delta):
+	$Player.rpc_unreliable("network_update", translation, rotation, $Player.is_crouching)
