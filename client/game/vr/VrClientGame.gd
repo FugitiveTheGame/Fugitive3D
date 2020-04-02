522543fc31aa79c60a974778fdaafd7df5a432d1
Adam Brown <adamwbrown@gmail.com>
2020-04-02 15:24:04 -0700

    VR crouching implemented

--- client/game/vr/VrClientGame.gd
+++ client/game/vr/VrClientGame.gd
@@ -1,6 +1,5 @@
 extends "res://client/game/ClientGame.gd"
 
-
 func create_player_seeker_node() -> Node:
 	var scene = preload("res://client/game/player/seeker/vr/VrClientSeeker.tscn")
 	return scene.instance()
