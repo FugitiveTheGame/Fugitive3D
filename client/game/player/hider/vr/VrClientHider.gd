9ec105572ba16f32731045c460fabb90a16886a4
Adam Brown <adamwbrown@gmail.com>
2020-04-02 15:24:04 -0700

    VR crouching implemented

--- client/game/player/hider/vr/VrClientHider.gd
+++ client/game/player/hider/vr/VrClientHider.gd
@@ -1,8 +1,1 @@
 extends "res://client/game/player/controller/vr/VrPlayerController.gd"
-
-func _ready():
-	$Player.set_is_local_player()
-
-
-func _physics_process(delta):
-	$Player.rpc_unreliable("network_update", translation, rotation)
--- client/game/player/seeker/vr/VrClientSeeker.gd
+++ client/game/player/seeker/vr/VrClientSeeker.gd
@@ -1,8 +1,5 @@
 extends "res://client/game/player/controller/vr/VrPlayerController.gd"
 
-func _ready():
-	$Player.set_is_local_player()
-
 
 func _physics_process(delta):
 	# Copy the hand attitude to the flashlight
