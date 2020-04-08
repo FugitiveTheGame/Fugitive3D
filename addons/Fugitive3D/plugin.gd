4396b5bed7c21b01ec6f2780942ec63b3806cb66
Adam Brown <adamwbrown@gmail.com>
2020-04-08 14:45:47 -0700

    Moved PlayerSpawn node it it's own directory and named it correctly

--- addons/Fugitive3D/plugin.gd
+++ addons/Fugitive3D/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("PlayerSpawn", "Spatial", preload("res://common/game/mode/HiderSpawn.gd"), preload("res://common/game/mode/player_spawn_icon.png"))
+	add_custom_type("PlayerSpawn", "Spatial", preload("res://common/game/mode/player_spawn/PlayerSpawn.gd"), preload("res://common/game/mode/player_spawn/player_spawn_icon.png"))
 
 
 func _exit_tree():
