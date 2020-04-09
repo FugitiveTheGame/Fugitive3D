4cbdd49ea894853133b1f84342ca8cbf177bc497
Adam Brown <adamwbrown@gmail.com>
2020-04-09 13:13:23 -0700

    Clients can now specify port before connecting

--- client/main_menu/MainMenu.gd
+++ client/main_menu/MainMenu.gd
@@ -7,13 +7,15 @@ func _ready():
 
 func _on_ConnectButton_pressed():
 	var ip := $ServerIpLabel/ServerIp.text as String
+	var portStr := $ServerPortLabel/ServerPort.text as String
+	var port := int(portStr)
 	var playerName := $PlayerNameLabel/PlayerName.text as String
-	connect_to_server(playerName, ip)
+	connect_to_server(playerName, ip, port)
 
 
-func connect_to_server(playerName: String, serverIp: String):
+func connect_to_server(playerName: String, serverIp: String, serverPort: int):
 	vr.log_info("connect_to_server")
-	ClientNetwork.join_game(serverIp, playerName)
+	ClientNetwork.join_game(serverIp, serverPort, playerName)
 
 
 func on_connected_to_server():
