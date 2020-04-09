41f164ab728f13ebb2f0d346da33151617601a00
Adam Brown <adamwbrown@gmail.com>
2020-04-09 13:07:15 -0700

    Default Server port is now 31000
    Server now accepts command line argument to specify port

--- server/lobby/ServerLobby.gd
+++ server/lobby/ServerLobby.gd
@@ -2,7 +2,8 @@ extends "res://common/lobby/Lobby.gd"
 
 func _ready():
 	if not ServerNetwork.is_hosting():
-		if not ServerNetwork.host_game():
+		var port := get_port()
+		if not ServerNetwork.host_game(port):
 			print("Failed to start server, shutting down.")
 			get_tree().quit()
 			return
@@ -12,3 +13,24 @@ func _ready():
 
 func on_start_game():
 	get_tree().change_scene("res://server/game/ServerGame.tscn")
+
+
+# Parse command line port in the form of:
+# --port xxxxx
+func get_port() -> int:
+	var port := ServerNetwork.SERVER_PORT
+	
+	var args := OS.get_cmdline_args()
+	for ii in range(args.size()):
+		var arg = args[ii]
+		if arg.nocasecmp_to("--port") == 0:
+			var next = ii+1
+			if args.size() > next:
+				var newPortStr = args[next]
+				var newPort := int(newPortStr)
+				if newPort != 0:
+					port = newPort
+					print("User specified port: %d" % port)
+					break
+	
+	return port
