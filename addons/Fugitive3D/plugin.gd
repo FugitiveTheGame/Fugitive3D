402b520a5629ab475ee496cb96bb24fb4706dd56
Adam Brown <adamwbrown@gmail.com>
2020-04-09 14:03:04 -0700

    Plugin should not pre-load scripts

--- addons/Fugitive3D/plugin.gd
+++ addons/Fugitive3D/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("PlayerSpawn", "Spatial", preload("res://common/game/mode/player_spawn/PlayerSpawn.gd"), preload("res://common/game/mode/player_spawn/player_spawn_icon.png"))
+	add_custom_type("PlayerSpawn", "Spatial", load("res://common/game/mode/player_spawn/PlayerSpawn.gd"), load("res://common/game/mode/player_spawn/player_spawn_icon.png"))
 
 
 func _exit_tree():
