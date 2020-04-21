75f01529e475b22e81be8cc007cf40fcd142f6d6
Adam Brown <adamwbrown@gmail.com>
2020-04-20 19:01:28 -0700

    VR players can now drive the car!

--- client/game/player/controller/vr/Locomotion_Stick.gd
+++ client/game/player/controller/vr/Locomotion_Stick.gd
@@ -1,7 +1,14 @@
 extends "res://addons/OQ_Toolkit/OQ_ARVROrigin/scripts/Locomotion_Stick.gd"
 
 var allowMovement := true
+var allowTurn := true
+
 
 func move(dt):
 	if allowMovement:
 		.move(dt)
+
+
+func turn(dt):
+	if allowTurn:
+		.turn(dt)
