2d31071e2836221703fb777f53b4d9f40de51523
Adam Brown <adamwbrown@gmail.com>
2020-04-22 15:38:13 -0700

    Added Frozen and Safe HUD indicators

---
+++ client/game/mode/fugitive/hider/SafeZoneHud.gd
@@ -0,0 +1,25 @@
+extends Control
+
+export(NodePath) var playerPath: NodePath
+onready var player := get_node(playerPath) as Hider
+
+onready var label := $SafeLabel as Label
+
+
+# If we are in a winzone, show the indicator
+func _physics_process(delta):
+	var isInWinZone := false
+	
+	var body := player.playerBody
+	var winZones = GameData.currentMap.get_win_zones()
+	for zone in winZones:
+		if zone.overlaps_body(body):
+			isInWinZone = true
+			break
+	
+	if isInWinZone:
+		if not label.visible:
+			label.show()
+	else:
+		if label.visible:
+			label.hide()
