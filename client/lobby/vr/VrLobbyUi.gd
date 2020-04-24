479765b7f6457c641952c227a1516c894f76b747
Adam Brown <adamwbrown@gmail.com>
2020-04-23 22:06:30 -0700

    VR scene fade

--- client/lobby/vr/VrLobbyUi.gd
+++ client/lobby/vr/VrLobbyUi.gd
@@ -2,9 +2,9 @@ extends "res://client/lobby/ClientLobby.gd"
 
 func on_start_game():
 	vr.log_info("on_start_game")
-	get_tree().change_scene("res://client/game/mode/fugitive/VrClientFugitiveGame.tscn")
+	vr.switch_scene("res://client/game/mode/fugitive/VrClientFugitiveGame.tscn")
 
 
 func on_disconnect():
 	vr.log_info("on_disconnect")
-	get_tree().change_scene("res://client/main_menu/vr/VrClientMainMenu.tscn")
+	vr.switch_scene("res://client/main_menu/vr/VrClientMainMenu.tscn")
--- client/main_menu/vr/VrClientMainMenuUi.gd
+++ client/main_menu/vr/VrClientMainMenuUi.gd
@@ -1,8 +1,8 @@
 extends "res://client/main_menu/MainMenu.gd"
 
 func go_to_lobby():
-	get_tree().change_scene("res://client/lobby/vr/VrLobby.tscn")
+	vr.switch_scene("res://client/lobby/vr/VrLobby.tscn")
 
 
 func _on_DebugButton_pressed():
-	get_tree().change_scene("res://client/game/mode/fugitive/FugitiveGame-dev-vr.tscn")
+	vr.switch_scene("res://client/game/mode/fugitive/FugitiveGame-dev-vr.tscn")
