67eb149a6e177d5149f24fe80deaafd392e1c484
Adam Brown <adamwbrown@gmail.com>
2020-04-23 21:32:05 -0700

    Added debug scene for VR

--- client/main_menu/vr/VrClientMainMenuUi.gd
+++ client/main_menu/vr/VrClientMainMenuUi.gd
@@ -2,3 +2,7 @@ extends "res://client/main_menu/MainMenu.gd"
 
 func go_to_lobby():
 	get_tree().change_scene("res://client/lobby/vr/VrLobby.tscn")
+
+
+func _on_DebugButton_pressed():
+	get_tree().change_scene("res://client/game/mode/fugitive/FugitiveGame-dev-vr.tscn")
