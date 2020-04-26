9c2922601cac4f7f400ee0c34a021e4eee224156
Adam Brown <adamwbrown@gmail.com>
2020-04-26 15:23:44 -0700

    Ensure we can end game from any state

--- common/game/mode/fugitive/FugitiveStateMachine.gd
+++ common/game/mode/fugitive/FugitiveStateMachine.gd
@@ -15,6 +15,10 @@ const TRANS_START_COUNT := "start_count"
 const TRANS_GAME_START := "game_start"
 const TRANS_COPS_RELEASED := "cops_released"
 const TRANS_END_GAME_EARLY := "end_game_early"
+const TRANS_END_GAME_ABORT_1 := "end_game_abort_1"
+const TRANS_END_GAME_ABORT_2 := "end_game_abort_2"
+const TRANS_END_GAME_ABORT_3 := "end_game_abort_3"
+const TRANS_END_GAME_ABORT_4 := "end_game_abort_4"
 const TRANS_END_GAME := "end_game"
 
 
@@ -34,5 +38,9 @@ func _ready():
 	create_transition(TRANS_COPS_RELEASED, playing_head_start, playing)
 	create_transition(TRANS_END_GAME, playing, game_over)
 	create_transition(TRANS_END_GAME_EARLY, playing_head_start, game_over)
+	create_transition(TRANS_END_GAME_ABORT_1, countdown, game_over)
+	create_transition(TRANS_END_GAME_ABORT_2, ready, game_over)
+	create_transition(TRANS_END_GAME_ABORT_3, not_ready, game_over)
+	create_transition(TRANS_END_GAME_ABORT_4, configuring, game_over)
 	
 	set_initial_state(configuring)
