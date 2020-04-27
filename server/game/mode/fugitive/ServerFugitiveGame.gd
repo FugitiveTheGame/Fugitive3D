b6e2d2a5cc3fc12aa9c59d4362647200f28b3a7a
Adam Brown <adamwbrown@gmail.com>
2020-04-27 16:20:41 -0700

    configure and ready phases are now much more robust
    People can join in leave during these phases and things won't break anymore

--- server/game/mode/fugitive/ServerFugitiveGame.gd
+++ server/game/mode/fugitive/ServerFugitiveGame.gd
@@ -1,8 +1,25 @@
 extends FugitiveGame
 
 
-var unconfiguredPlayers := {}
-var unreadyPlayers := {}
+var configuredPlayers := {}
+var readyPlayers := {}
+
+func unconfigured_players() -> int:
+	var count := 0
+	for playerId in GameData.players:
+		if not configuredPlayers.has(playerId):
+			count += 1
+	
+	return count
+
+
+func not_ready_players() -> int:
+	var count := 0
+	for playerId in GameData.players:
+		if not readyPlayers.has(playerId):
+			count += 1
+	
+	return count
 
 
 func server_remove_player(playerId: int):
@@ -12,13 +29,15 @@ func server_remove_player(playerId: int):
 		get_tree().change_scene("res://server/lobby/ServerLobby.tscn")
 	else:
 		print("Players remaining: %d" % GameData.players.size())
+		
+		if stateMachine.current_state.name == FugitiveStateMachine.STATE_CONFIGURING:
+			check_all_configured()
+		elif stateMachine.current_state.name == FugitiveStateMachine.STATE_NOT_READY:
+			check_all_ready()
 
 
 func _ready():
 	ClientNetwork.connect("remove_player", self, "server_remove_player")
-	
-	for playerId in GameData.players:
-		unconfiguredPlayers[playerId] = playerId
 
 
 func load_map(mapPath: String):
@@ -29,26 +48,34 @@ func load_map(mapPath: String):
 
 remote func on_client_configured(playerId: int):
 	print("client configured: %s" % playerId)
-	unconfiguredPlayers.erase(playerId)
-	unreadyPlayers[playerId] = playerId
-	print("Still waiting on %d players" % unconfiguredPlayers.size())
+	configuredPlayers[playerId] = true
 	
+	check_all_configured()
+
+func check_all_configured():
 	# All clients are done, unpause the game
-	if unconfiguredPlayers.empty():
+	if unconfigured_players() == 0:
 		print("Waiting for players to ready up...")
 		rpc("on_all_clients_configured")
+	else:
+		print("Still waiting on %d players" % unconfigured_players())
 
 
 remote func on_client_ready(playerId: int):
 	print("client ready: %s" % playerId)
-	if unreadyPlayers.erase(playerId):
-		print("Still waiting on %d players" % unreadyPlayers.size())
-		
-		# All clients are done, unpause the game
-		if unreadyPlayers.empty():
-			stateMachine.transition_by_name(FugitiveStateMachine.TRANS_READY)
-			print("Starting the countdown")
-			rpc("on_all_ready")
+	readyPlayers[playerId] = true
+	
+	check_all_ready()
+
+
+func check_all_ready():
+	if not_ready_players() > 0:
+		print("Still waiting on %d players" % not_ready_players())
+	# All clients are done, unpause the game
+	else:
+		stateMachine.transition_by_name(FugitiveStateMachine.TRANS_READY)
+		print("Starting the countdown")
+		rpc("on_all_ready")
 
 
 # Only the Server listens to the timer
