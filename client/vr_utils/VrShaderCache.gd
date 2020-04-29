f9f31f0919deef052e835cf74b1ffa9ce0d8e551
Adam Brown <adamwbrown@gmail.com>
2020-04-28 22:48:38 -0700

    VR shader cache work

--- client/vr_utils/VrShaderCache.gd
+++ client/vr_utils/VrShaderCache.gd
@@ -1,12 +1,10 @@
 extends Spatial
 
 func _ready():
-	#show()
-	# disable this for the moment as it does not appear to be working
-	pass
+	show()
 
 
 func _on_HideTimer_timeout():
 	for child in get_children():
 		if child is Spatial:
-			child.hide()
+			child.queue_free()
