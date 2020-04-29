8900fb83bc57c5811300786506cb7a988315fc8b
Adam Brown <adamwbrown@gmail.com>
2020-04-29 13:04:26 -0700

    Added the ability to change the movement hand in VR

--- client/game/player/controller/vr/Locomotion_Stick.gd
+++ client/game/player/controller/vr/Locomotion_Stick.gd
@@ -8,6 +8,18 @@ func _ready():
 	movmenet_orientation = UserData.data.vr_movement_orientation
 	enable_vignette = UserData.data.vr_movement_vignetting
 	
+	match UserData.data.vr_movement_hand:
+		0:
+			move_left_right = vr.AXIS.LEFT_JOYSTICK_X;
+			move_forward_back = vr.AXIS.LEFT_JOYSTICK_Y;
+			
+			turn_left_right = vr.AXIS.RIGHT_JOYSTICK_X;
+		1:
+			move_left_right = vr.AXIS.RIGHT_JOYSTICK_X;
+			move_forward_back = vr.AXIS.RIGHT_JOYSTICK_Y;
+			
+			turn_left_right = vr.AXIS.LEFT_JOYSTICK_X;
+	
 
 func move(dt):
 	if allowMovement:
