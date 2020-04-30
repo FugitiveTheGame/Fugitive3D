b699a6029e03fe51100afa53908049035caf0fb8
Adam Brown <adamwbrown@gmail.com>
2020-04-29 22:05:35 -0700

    Currently Vignetting only works on desktop

--- client/main_menu/vr/VrClientMainMenuUi.gd
+++ client/main_menu/vr/VrClientMainMenuUi.gd
@@ -3,8 +3,11 @@ extends "res://client/main_menu/MainMenu.gd"
 export(NodePath) var settingsDialogPath: NodePath
 onready var settingsDialog := get_node(settingsDialogPath) as WindowDialog
 
+export(NodePath) var debugButtonPath: NodePath
+onready var debugButton := get_node(debugButtonPath) as Button
+
 func _ready():
-	$DebugButton.visible = OS.is_debug_build()
+	debugButton.visible = OS.is_debug_build()
 
 
 func go_to_lobby():
