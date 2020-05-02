37dfac9f6ee2ccc0ff0d68cc207834b119541ef9
Adam Brown <adamwbrown@gmail.com>
2020-05-02 15:24:18 -0700

    PlayerSpawn now indicates the direction
    the player will face

--- common/game/mode/player_spawn/PlayerSpawn.gd
+++ common/game/mode/player_spawn/PlayerSpawn.gd
@@ -3,6 +3,13 @@ extends Spatial
 
 func _ready():
 	if Engine.editor_hint:
-		var tool_time_mesh = CSGSphere.new()
+		var tool_time_mesh := CSGSphere.new()
 		tool_time_mesh.radius = 0.25
 		add_child(tool_time_mesh)
+		
+		var tool_time_mesh_directional := CSGBox.new()
+		tool_time_mesh_directional.width = 0.1
+		tool_time_mesh_directional.height = 0.1
+		tool_time_mesh_directional.depth = 0.5
+		add_child(tool_time_mesh_directional)
+		tool_time_mesh_directional.translate(Vector3(0.0, 0.0, -0.25))
