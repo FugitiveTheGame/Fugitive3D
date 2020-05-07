faaa4f8dba02a01180c4fc9795cf4c47a81cd87d
Adam Brown <adamwbrown@gmail.com>
2020-05-07 09:17:50 -0700

    Port check (#158)
    * Public registration now waits for repository to send a "ping" we respond with a "pong" and this verified that the port is globally accessible.
    * Lots of error messages to help server admins determine why they arent being registered.
    * Added --nolan for servers

---
+++ server/ServerUtils.gd
@@ -0,0 +1,81 @@
+extends Object
+class_name ServerUtils
+
+
+static func configure_advertiser(advertiser: ServerAdvertiser, _name, _port):
+	advertiser.serverInfo["port"] = _port
+	advertiser.serverInfo["name"] = _name
+	advertiser.serverInfo["game_version"] = UserData.GAME_VERSION
+	advertiser.serverRepositoryUrl = ServerNetwork.SERVER_REPOSITORY_URL
+
+
+# Parse command line server name in the form of:
+# --name xxxxx
+static func get_name() -> String:
+	var name := "Fugitive 3D Server"
+	
+	var args := OS.get_cmdline_args()
+	for ii in range(args.size()):
+		var arg = args[ii]
+		if arg.nocasecmp_to("--name") == 0:
+			var next = ii+1
+			if args.size() > next:
+				var newName = args[next]
+				if newName.length() > 0 and newName.length() < 32:
+					name = newName
+					print("User specified name: %s" % name)
+				else:
+					print("Invalid server name length")
+				
+				break
+	
+	return name
+
+
+# Parse command line port in the form of:
+# --port xxxxx
+static func get_port() -> int:
+	var port := ServerNetwork.SERVER_PORT
+	
+	var args := OS.get_cmdline_args()
+	for ii in range(args.size()):
+		var arg = args[ii]
+		if arg.nocasecmp_to("--port") == 0:
+			var next = ii+1
+			if args.size() > next:
+				var newPortStr = args[next]
+				var newPort := int(newPortStr)
+				if newPort != 0:
+					port = newPort
+					print("User specified port: %d" % port)
+					break
+	
+	return port
+
+
+# Parse command line port in the form of:
+# --public
+static func get_public() -> bool:
+	var public := false
+	
+	var args := OS.get_cmdline_args()
+	for arg in args:
+		if arg.nocasecmp_to("--public") == 0:
+			public = true
+			break
+	
+	return public
+
+
+# Parse command line server name in the form of:
+# --nolan
+static func get_no_lan() -> bool:
+	var no_lan := false
+	
+	var args := OS.get_cmdline_args()
+	for arg in args:
+		if arg.nocasecmp_to("--nolan") == 0:
+			no_lan = true
+			break
+	
+	return no_lan
