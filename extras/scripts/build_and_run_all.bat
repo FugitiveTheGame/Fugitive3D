b1393174653ef12607803f5c92f6fa6e41301e94
Adam Brown <adamwbrown@gmail.com>
2020-05-10 14:04:35 -0700

    Fixed build script

--- extras/scripts/build_and_run_all.bat
+++ extras/scripts/build_and_run_all.bat
@@ -2,12 +2,12 @@
 cd ../../
 
 :: Build the client
-taskkill /IM "FlatClient.exe" /F
-godot.windows.opt.tools.64.exe --export-debug "Client Flat - Windows" export/client/flat/windows/FlatClient.exe
+taskkill /IM "Fugitive3D_Client_Flat_Windows.exe" /F
+godot.windows.opt.tools.64.exe --export-debug "Client Flat - Windows" export/client/flat/windows/Fugitive3D_Client_Flat_Windows.exe
 
 :: Build the server
-taskkill /IM "Server.exe" /F
-godot.windows.opt.tools.64.exe --export-debug "Server - Windows" export/server/windows/Server.exe
+taskkill /IM "Fugitive3D_Server_Windows.exe" /F
+godot.windows.opt.tools.64.exe --export-debug "Server - Windows" export/server/windows/Fugitive3D_Server_Windows.exe
 
 :: Run a server with connected clients
 cd extras/scripts
