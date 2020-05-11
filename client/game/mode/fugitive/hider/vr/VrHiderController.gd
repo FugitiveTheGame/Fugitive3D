9cb49fefc6923c9b7b68c8f800cf871f7a9e0067
Adam Brown <adamwbrown@gmail.com>
2020-05-11 13:44:34 -0700

    When frozen, hiders now see map automatically

--- client/game/mode/fugitive/hider/vr/VrHiderController.gd
+++ client/game/mode/fugitive/hider/vr/VrHiderController.gd
@@ -1,1 +1,9 @@
 extends "res://client/game/mode/fugitive/VrFugitiveController.gd"
+
+
+# Hiders auto-show the map when frozen
+func show_map(show: bool):
+	if player.frozen:
+		overviewMapHud.visible = true
+	else:
+		overviewMapHud.visible = show
