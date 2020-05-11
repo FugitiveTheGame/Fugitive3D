f6254295360e1a21369353a376c301b004058748
Adam Brown <adamwbrown@gmail.com>
2020-05-11 15:23:21 -0700

    EndGame stats UI got a polish pass

--- client/game/mode/fugitive/hud/EndGameHud.gd
+++ client/game/mode/fugitive/hud/EndGameHud.gd
@@ -2,55 +2,66 @@ extends Panel
 
 export(NodePath) var playerListPath: NodePath
 onready var playerList := get_node(playerListPath) as VBoxContainer
+
 export(NodePath) var replayScrubSliderPath: NodePath
 onready var replayScrubSlider := get_node(replayScrubSliderPath) as HSlider
+
 export(NodePath) var replaySpeedSliderPath: NodePath
 onready var replaySpeedSlider := get_node(replaySpeedSliderPath) as HSlider
+
 export(NodePath) var replayPlayButtonPath: NodePath
 onready var replayPlayButton := get_node(replayPlayButtonPath) as Button
+
 export(NodePath) var replayLabelHistoryPath: NodePath
 onready var replayLabelHistory := get_node(replayLabelHistoryPath) as Label
+
 export(NodePath) var replayLabelSpeedPath: NodePath
 onready var replayLabelSpeed := get_node(replayLabelSpeedPath) as Label
+
 export(NodePath) var replayHudPath: NodePath
 onready var replayHud := get_node(replayHudPath) as HistoryMapHud
 
+
 func _ready():
 	hide()
+
 
 func team_won(winningTeam: int):
 	
 	var winningTeamName = GameData.currentGame.get_team_name(winningTeam)
 	$Container/WinnerLabel.text = "%ss won!" % winningTeamName
 	
-	for playerInfo in GameData.get_players():
-		var playerInfoData := playerInfo as PlayerData
-		var node := Label.new()
-		var playerType := playerInfoData.get_type()
-		var team = GameData.currentGame.get_team_name(playerType)
-		
-		var playerObj := GameData.currentGame.get_player(playerInfoData.get_id()) as FugitivePlayer
-		if playerObj != null:
-			var frozen: String
-			if playerObj.frozen:
-				frozen = "Captured"
-			else:
-				frozen = "ESCAPED!"
-			
-			if playerType == FugitiveTeamResolver.PlayerType.Hider:
-				node.text = "[%s] %s - %s" % [team, playerInfo.get_name(), frozen]
-			else:
-				node.text = "[%s] %s" % [team, playerInfo.get_name()]
-				
-			var playerStats = FugitivePlayerDataUtility.get_match_stats(playerInfoData)
-			print("Player stat count: %d" % playerStats.size())
-			for statKey in playerStats.keys():
-				var statName := FugitivePlayerDataUtility.get_stat_name(statKey)
-				var stat := FugitivePlayerDataUtility.get_match_stat(playerInfoData, statKey)
-				node.text += "\n\t%s: %d" % [statName, stat]
-			
-			playerList.add_child(node)
+	var headerListItem := preload("res://client/game/mode/fugitive/hud/StatsHeader.tscn")
+	var playerStatsListItemScene := preload("res://client/game/mode/fugitive/hud/PlayerStatsListItem.tscn")
 	
+	var seekers = get_tree().get_nodes_in_group(Seeker.GROUP)
+	var hiders = get_tree().get_nodes_in_group(Hider.GROUP)
+	
+	# Rendering seekers
+	var header := headerListItem.instance()
+	header.set_text( FugitiveTeamResolver.get_team_name(FugitiveTeamResolver.PlayerType.Seeker) + "s" )
+	playerList.add_child(header)
+	
+	for playerObj in seekers:
+		var playerInfoData := GameData.get_player(playerObj.id)
+		if playerObj != null:
+			var playerStatsListItem := playerStatsListItemScene.instance()
+			playerStatsListItem.populate(playerInfoData, playerObj)
+			playerList.add_child(playerStatsListItem)
+	
+	# Render hiders
+	header = headerListItem.instance()
+	header.set_text( FugitiveTeamResolver.get_team_name(FugitiveTeamResolver.PlayerType.Hider) + "s" )
+	playerList.add_child(header)
+	
+	for playerObj in hiders:
+		var playerInfoData := GameData.get_player(playerObj.id)
+		if playerObj != null:
+			var playerStatsListItem := playerStatsListItemScene.instance()
+			playerStatsListItem.populate(playerInfoData, playerObj)
+			playerList.add_child(playerStatsListItem)
+	
+	# Setup history map
 	replayHud.stop()
 	replayHud.setIndex(0)
 	replayScrubSlider.value = 0
@@ -62,20 +73,25 @@ func team_won(winningTeam: int):
 	
 	$GameOverAudio.play()
 
+
 func _process(delta):
 	replayScrubSlider.value = replayHud.getIndex()
 	replayLabelHistory.text = "History: %fs" % replayScrubSlider.value
 
+
 func _on_ToLobbyButton_pressed():
 	GameData.currentGame.go_to_lobby()
+
 
 func _on_Scrub_value_changed(value):
 	replayHud.setIndex(int(round(value)))
 	replayLabelHistory.text = "History: %fs" % value
 
+
 func _on_Speed_value_changed(value):
 	replayHud.setFrameSpeed(value)
 	replayLabelSpeed.text = "Speed: %fx" % value
+
 
 func _on_ButtonHistoryPlay_pressed():
 	if replayHud.togglePlay():
