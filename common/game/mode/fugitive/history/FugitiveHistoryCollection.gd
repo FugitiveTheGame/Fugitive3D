72f7492bf9754804053bc5a74777cbee2413deb7
Adam Brown <adamwbrown@gmail.com>
2020-05-16 13:30:30 -0700

    History heartbeats are now all local. No bandwidth required!

--- common/game/mode/fugitive/history/FugitiveHistoryCollection.gd
+++ common/game/mode/fugitive/history/FugitiveHistoryCollection.gd
@@ -4,11 +4,14 @@ class_name FugitiveHistoryCollection
 var stateHistoryArray := []
 var player_summaries := {}
 
-remotesync func on_history_heartbeat(heartbeat: Dictionary):
+
+func record_heartbeat(heartbeat: Dictionary):
 	stateHistoryArray.append(heartbeat)
+
 
 func reset():
 	stateHistoryArray.clear()
+
 
 func loadPlayers(players : Dictionary):
 	player_summaries = players.duplicate()
