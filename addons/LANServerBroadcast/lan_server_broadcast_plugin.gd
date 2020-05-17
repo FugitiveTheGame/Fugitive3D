84add7f486223cd3c3036ae77587b4ef225b4725
Adam Brown <adamwbrown@gmail.com>
2020-05-17 15:29:26 -0700

    Maybe this will make it happy...

--- addons/LANServerBroadcast/lan_server_broadcast_plugin.gd
+++ addons/LANServerBroadcast/lan_server_broadcast_plugin.gd
@@ -2,8 +2,8 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("ServerAdvertiser", "Node", preload("server_advertiser/ServerAdvertiser.gd"), preload("server_advertiser/ServerAdvertiser.png"))
-	add_custom_type("ServerListener", "Node", preload("server_listener/ServerListener.gd"), preload("server_listener/ServerListener.png"))
+	add_custom_type("ServerAdvertiser", "Node", load("server_advertiser/ServerAdvertiser.gd"), load("server_advertiser/ServerAdvertiser.png"))
+	add_custom_type("ServerListener", "Node", load("server_listener/ServerListener.gd"), load("server_listener/ServerListener.png"))
 
 
 func _exit_tree():
