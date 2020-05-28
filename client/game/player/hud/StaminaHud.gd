8341554e99821e609d29b91f0520258596a1b6a1
Adam Brown <adamwbrown@gmail.com>
2020-05-28 15:34:01 -0700

    Reworked stamina
    Now bottoming out results in exhaustion which reduces your max walk speed and prevents sprinting.
    Cops also now move the same speed.

--- client/game/player/hud/StaminaHud.gd
+++ client/game/player/hud/StaminaHud.gd
@@ -2,8 +2,13 @@ extends Control
 
 export(NodePath) var playerPath: NodePath
 onready var player := get_node(playerPath) as Player
-onready var staminaBar := $ProgressBar
+onready var staminaBar := $ProgressBar as ProgressBar
+onready var icon := $TextureRect as TextureRect
 
+
+var icon_stamina := load("res://client/game/player/hud/ic_stamina.png")
+var icon_exhausted := load("res://client/game/mode/fugitive/hud/ic_exhausted.webp")
+var showing_exhausted := false
 
 func _ready():
 	staminaBar.max_value = player.stamina_max
@@ -11,3 +16,12 @@ func _ready():
 
 func _process(delta):
 	staminaBar.value = player.stamina
+	
+	if player.exhausted:
+		if not showing_exhausted:
+			icon.texture = icon_exhausted
+			showing_exhausted = true
+	else:
+		if showing_exhausted:
+			icon.texture = icon_stamina
+			showing_exhausted = false
