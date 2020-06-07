545c72d774a5f31b21db490b4372810416e9e026
Adam Brown <adamwbrown@gmail.com>
2020-06-07 13:16:59 -0700

    Made Voice chat nodes more generic
    - Different game modes will have different transmit logic
    - Lobby needs to not be 3D audio

--- common/voice_chat/VoiceChatTransceiver.gd
+++ common/voice_chat/VoiceChatTransceiver.gd
@@ -42,26 +42,10 @@ func transmit_audio():
 		send_audio(recording.data)
 
 
+# This should be overriden to determine who the audio is sent to based on game
+# rules
 func send_audio(audioData: PoolByteArray):
-	var encodedData = opus_encoder.encode(audioData)
-	
-	var localPlayer := GameData.currentGame.localPlayer
-	var localPlayerPos := localPlayer.global_transform.origin
-	
-	for playerId in GameData.currentGame.players:
-		if playerId != GameData.currentGame.localPlayer.id:
-			var player := GameData.currentGame.players[playerId] as Player
-			
-			if player.is_on_local_players_team():
-				# Team mate is within ear shot
-				if localPlayerPos.distance_to(player.global_transform.origin) <= maxTeamHearingRange:
-					rpc_id(playerId,"on_receive_audio", encodedData)
-				# If team member is out of range, hear it locally as if by radio
-				else:
-					player.playerVoiceChat.rpc_unreliable_id(playerId,"on_receive_audio", encodedData)
-			# Enemy is within ear shot
-			elif localPlayerPos.distance_to(player.global_transform.origin) <= maxHearingRange:
-				rpc_id(playerId,"on_receive_audio", encodedData)
+	pass
 
 
 func _on_TransmitLimitTimer_timeout():
