8566d9f2356b1c72f60332c3c7b9bd2fb95b0136
Adam Brown <adamwbrown@gmail.com>
2020-06-07 14:11:46 -0700

    More voip fixes

--- common/lobby/LocalPlayerListItem.gd
+++ common/lobby/LocalPlayerListItem.gd
@@ -1,8 +1,8 @@
 extends "res://common/lobby/PlayerListItem.gd"
 
 
-onready var voice_chat := $VoiceChat as VoiceChatReceiver
+onready var voice_chat := $VoiceChat as VoiceChatTransceiver
 
 
 func is_voip_active() -> bool:
-	return voice_chat.is_playing()
+	return voice_chat.is_recording()
