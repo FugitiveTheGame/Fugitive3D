ce4b12fae8d17b37813e02b249ad207f318d1295
Adam Brown <adamwbrown@gmail.com>
2020-06-12 14:26:52 -0700

    Fixed perf issues related to the Ambience system
    Sadly, Mobile can't handle fireflies :(

--- common/game/mode/fugitive/ambience/OneTimeAudioEffect.gd
+++ common/game/mode/fugitive/ambience/OneTimeAudioEffect.gd
@@ -1,10 +1,13 @@
 extends AudioStreamPlayer3D
 class_name OneTimeAudioEffect
 
+signal audio_effect_complete(node)
+
 
 func _ready():
 	connect("finished", self, "on_finished")
+	play()
 
 
 func on_finished():
-	queue_free()
+	emit_signal("audio_effect_complete", self)
