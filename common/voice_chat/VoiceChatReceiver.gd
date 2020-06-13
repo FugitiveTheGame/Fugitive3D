4265874c5d78ada8c5266168453d67edcd2ca2cd
Adam Brown <adamwbrown@gmail.com>
2020-06-13 15:27:16 -0700

    Prevent voip clips from stomping over each other

--- common/voice_chat/VoiceChatReceiver.gd
+++ common/voice_chat/VoiceChatReceiver.gd
@@ -6,6 +6,9 @@ onready var audioPlayer := get_node(audioPlayerPath)
 
 onready var opus_decoder := $OpusDecoder
 
+const MAX_CLIPS := 5
+var audio_clips := []
+
 
 func _ready():
 	var audioStream := AudioStreamSample.new()
@@ -14,6 +17,8 @@ func _ready():
 	audioStream.mix_rate = 44100
 	
 	audioPlayer.stream = audioStream
+	
+	audioPlayer.connect("finished", self, "on_audio_finished")
 
 
 func send_audio(audioData: PoolByteArray):
@@ -23,9 +28,24 @@ func send_audio(audioData: PoolByteArray):
 remote func on_receive_audio(audioData: PoolByteArray):
 	var pcm_data = opus_decoder.decode(audioData)
 	
-	audioPlayer.stream.data = pcm_data
-	audioPlayer.play()
+	if audio_clips.size() < MAX_CLIPS:
+		audio_clips.push_back(pcm_data)
+		play_next_clip()
+	else:
+		print("Dropping audio clip, too many in queue already")
 
 
 func is_playing() -> bool:
 	return audioPlayer.playing as bool
+
+
+func play_next_clip():
+	if not is_playing() and not audio_clips.empty():
+		var clip = audio_clips.pop_front()
+		
+		audioPlayer.stream.data = clip
+		audioPlayer.play()
+
+
+func on_audio_finished():
+	play_next_clip()
