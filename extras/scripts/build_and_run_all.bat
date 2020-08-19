c24447a087b9563e1495ef7b419eb03d7679e91c
Stacy Brown <thatstacybrown@gmail.com>
2020-08-18 19:12:56 -0700

    build script now creates dirs

--- extras/scripts/build_and_run_all.bat
+++ extras/scripts/build_and_run_all.bat
@@ -3,10 +3,12 @@ cd ../../
 
 :: Build the client
 taskkill /IM "Fugitive3D_Client_Flat_Windows.exe" /F
+if not exist "export\client\flat\windows" mkdir export\client\flat\windows
 Godot_v3.2.2-stable_win64.exe --export-debug "Client Flat - Windows" export/client/flat/windows/Fugitive3D_Client_Flat_Windows.exe
 
 :: Build the server
 taskkill /IM "Fugitive3D_Server_Windows.exe" /F
+if not exist "export\server\windows" mkdir export\server\windows
 Godot_v3.2.2-stable_win64.exe --export-debug "Server - Windows" export/server/windows/Fugitive3D_Server_Windows.exe
 
 :: Run a server with connected clients
