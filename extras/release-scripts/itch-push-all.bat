ab3b4bf2d0abb421954f482c468b4a806522024f
Adam Brown <adamwbrown@gmail.com>
2020-09-06 12:48:05 -0700

    Version bump

--- extras/release-scripts/itch-push-all.bat
+++ extras/release-scripts/itch-push-all.bat
@@ -19,16 +19,19 @@ echo --=== Setup Windows Server DLLs ===--
 powershell Copy-Item -Path ../win-dependencies/vcruntime140.dll -Destination ../../export/server/windows/
 powershell Copy-Item -Path ../win-dependencies/vcruntime140_1.dll -Destination ../../export/server/windows/
 
+:CLIENTS_FLAT
 echo --=== Flat clients ===--
 butler push --ignore godot_oculus.dll --ignore libgodot_openvr.dll --ignore openvr_api.dll ..\..\export\client\flat\windows stumpy-dog-studios/fugitive-3d:windows-flat --userversion-file vesion.txt
 butler push --ignore libgodot_openvr.so --ignore libopenvr_api.so ..\..\export\client\flat\linux stumpy-dog-studios/fugitive-3d:linux-flat --userversion-file vesion.txt
 butler push ..\..\export\client\flat\osx stumpy-dog-studios/fugitive-3d:osx-flat --userversion-file vesion.txt
 butler push releases/Fugitive3D_Client_Flat_Android.apk stumpy-dog-studios/fugitive-3d:android-flat --userversion-file vesion.txt
 
+:CLIENTS_VR
 echo --=== VR Clients ===--
 butler push ..\..\export\client\vr\windows stumpy-dog-studios/fugitive-3d:windows-vr --userversion-file vesion.txt
 butler push releases/Fugitive3D_Client_VR_Quest.apk stumpy-dog-studios/fugitive-3d:quest-vr --userversion-file vesion.txt
 
+:SERVERS
 echo --=== Servers ===--
 butler push --ignore godot_oculus.dll --ignore libgodot_openvr.dll --ignore openvr_api.dll ..\..\export\server\windows stumpy-dog-studios/fugitive-3d:windows-server --userversion-file vesion.txt
 butler push --ignore libgodot_openvr.so --ignore libopenvr_api.so ..\..\export\server\linux stumpy-dog-studios/fugitive-3d:linux-server --userversion-file vesion.txt
