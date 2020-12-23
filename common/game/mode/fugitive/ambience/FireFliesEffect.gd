5aa5d376911190436974c60562f60d211627adda
Adam Brown <adamwbrown@gmail.com>
2020-12-22 21:19:08 -0800

    Remove fireflies again for Quest 1

--- common/game/mode/fugitive/ambience/FireFliesEffect.gd
+++ common/game/mode/fugitive/ambience/FireFliesEffect.gd
@@ -2,8 +2,12 @@ extends AmbientVisualEffect
 
 
 func initialize_effect(player):
-	# Move the 
-	var fireFliesParticles = $FireFliesParticles
-	assert(fireFliesParticles != null)
-	fireFliesParticles.get_parent().remove_child(fireFliesParticles)
-	player.add_child(fireFliesParticles)
+	# Quest 1 is not powerful enough, remove the fireflies
+	if Utils.is_quest1():
+		$FireFliesParticles.queue_free()
+	else:
+		# Move the effect to the player so it follows them
+		var fireFliesParticles = $FireFliesParticles
+		assert(fireFliesParticles != null)
+		fireFliesParticles.get_parent().remove_child(fireFliesParticles)
+		player.add_child(fireFliesParticles)
