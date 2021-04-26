54a74cca9eb3624b4ce95ea8be1b265e770d0e92
Adam Brown <adamwbrown@gmail.com>
2021-04-26 16:32:03 -0700

    Fixed server Threshold

--- common/Threshold.gd
+++ common/Threshold.gd
@@ -4,6 +4,7 @@ class_name Threshold
 
 var threshold_ms := 1_000
 
+var is_running := false
 
 var last_run := 0 setget set_last_run
 func set_last_run(v: int):
@@ -14,6 +15,14 @@ func _init(threshold):
 	threshold_ms = threshold
 
 
+func start():
+	is_running = true
+
+
+func stop():
+	is_running = false
+
+
 func reset():
 	last_run = 0
 
@@ -21,10 +30,13 @@ func reset():
 func is_exceeded() -> bool:
 	var exceeded: bool
 	
-	var cur_time := OS.get_system_time_msecs()
-	if (cur_time - last_run) >= threshold_ms:
-		last_run = cur_time
-		exceeded = true
+	if is_running:
+		var cur_time := OS.get_system_time_msecs()
+		if (cur_time - last_run) >= threshold_ms:
+			last_run = cur_time
+			exceeded = true
+		else:
+			exceeded = false
 	else:
 		exceeded = false
 	
