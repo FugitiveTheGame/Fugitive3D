aad96dd0f72c72aa0e40287699d577d06e237bab
Adam Brown <adamwbrown@gmail.com>
2021-07-09 17:51:06 -0700

    Fix VR builds

--- addons/OQ_Toolkit/OQ_ARVROrigin/scripts/Feature_PlayerCollision.gd
+++ addons/OQ_Toolkit/OQ_ARVROrigin/scripts/Feature_PlayerCollision.gd
@@ -11,9 +11,7 @@ onready var collision_object = $CollisionShape;
 func _ready():
 	if (not get_parent() is ARVROrigin):
 		vr.log_error("Feature_StickMovement: parent is not ARVROrigin");
-	
-	capsule_radius = collision_object.shape.radius
-	
+		
 	_update_collsion_shape_start_position();
 
 
@@ -21,6 +19,7 @@ func _show_debug_information():
 	var slide_count = get_slide_count();
 	var on_floor = 1 if is_on_floor() else 0;
 	var on_wall = 1 if is_on_wall() else 0;
+	
 	
 	var colliders = "";
 	for c in range(0, slide_count):
@@ -30,15 +29,10 @@ func _show_debug_information():
 	vr.show_dbg_info("Feature_PlayerCollision", "Slide Count: %d; on floor: %d; on wall: %d; last update: %d;\n         Colliders: %s" % [slide_count, on_floor, on_wall, vr.frame_counter, colliders]);
 
 
-func _physics_process(delta):
-	_update_collsion_shape_start_position()
-
-
 func _update_collsion_shape_start_position():
 	var player_height = vr.get_current_player_height();
 	collision_object.shape.radius = capsule_radius;
 	collision_object.shape.height = player_height - 2.0 * capsule_radius - step_offset;
-	
 	global_transform.origin = vr.vrCamera.global_transform.origin;
 	global_transform.origin.y -= (player_height-step_offset) * 0.5;
 	
